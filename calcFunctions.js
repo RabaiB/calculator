8b043c734bdfb8322fe25f4790f280673dddc22b
RabaiB <58396171+RabaiB@users.noreply.github.com>
2025-11-03 22:14:35 +0100

    Implemented calculations into frontend

--- calcFunctions.js
+++ calcFunctions.js
@@ -11,5 +11,44 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) {
+        return "ERROR: Division by zero";
+    }
+
     return a / b;
+}
+
+function clearMemory() {
+    numbers.numberA = "";
+    numbers.numberB = "";
+    numbers.operator = "";
+    numbers.allowSecondNumber = false;
+}
+
+
+function operate(numA, numB, operator) {
+    let result;
+
+        if (numA != "" && numB != "") {
+
+            switch (operator) {
+            case '+':
+                result =  add(numA, numB)
+                break;
+
+            case '-':
+                result = subtract(numA, numB);
+                break;
+                
+            case '*':
+                result = multiply(numA, numB);
+                break;
+                
+            case '/':
+                result = divide(numA, numB);
+                break;                
+        }
+        }
+
+    return result;
 }
